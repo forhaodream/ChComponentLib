1653b593a60cb1d7587018cd3047e5fefef69dae
ch <ch00209@163.com>
2019-09-18 09:40:35 +0800

    一个新的组件库

---
+++ chtool/src/main/java/ch/chtool/recycler/AAAA.java
@@ -0,0 +1,50 @@
+package ch.chtool.recycler;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+
+import com.squareup.okhttp.Request;
+
+import ch.chtool.R;
+import ch.chtool.okhttp.OkHttpClientManager;
+import ch.chtool.view.PopupWindowView;
+
+/**
+ * Created by CH
+ * on 2018/11/27 0027 09:36
+ */
+public class AAAA extends Activity {
+    PopupWindowView<String> popupWindowView;
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        popupWindowView = new PopupWindowView<String>(R.layout.aaaaaaaaaa, this) {
+            @Override
+            public void convert(String o, int position) {
+
+            }
+        };
+        TextView textView;
+        textView = findViewById(R.id.id_tag_autolayout_margin);
+        popupWindowView.showAsDropDown(textView, Gravity.BOTTOM, 0, 0);
+    }
+
+    private void aaa() {
+        OkHttpClientManager.postAsyn("", new OkHttpClientManager.ResultCallback<String>() {
+            @Override
+            public void onError(Request request, Exception e) {
+
+            }
+
+            @Override
+            public void onResponse(String response) {
+
+            }
+        });
+    }
+}
